17e5b7926fbc7c757bef94266127d1de1f1e940f
truman-show <ljh86091@gmail.com>
2020-11-24 22:39:19 +0900

    test PR 222

--- applications/admin-api/src/main/java/com/sweet/AdminApplication.java
+++ applications/admin-api/src/main/java/com/sweet/AdminApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminApplication {
 
+
+
   public static void main(String[] args) {
     SpringApplication.run(AdminApplication.class, args);
   }
+
+
+
 
 }
