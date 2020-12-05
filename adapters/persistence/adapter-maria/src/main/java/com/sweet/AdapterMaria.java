6790f7afe1731bf626184b5422d23e46b257d030
Kimyongjin <yongjin5184@gmail.com>
2020-12-05 14:21:17 +0900

    SpringBootApplication 추가

---
+++ adapters/persistence/adapter-maria/src/main/java/com/sweet/AdapterMaria.java
@@ -0,0 +1,13 @@
+package com.sweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterMaria {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdapterMaria.class, args);
+    }
+
+}
