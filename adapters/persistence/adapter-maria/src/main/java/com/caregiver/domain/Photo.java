bda93ac36d9b2e8a1852b68d12d61f22f3e47c9d
truman-show <65605968+truman-show@users.noreply.github.com>
2021-04-25 21:24:31 +0900

    package 를 변경하라 (#28)
    - sweet -> caregiver

---
+++ adapters/persistence/adapter-maria/src/main/java/com/caregiver/domain/Photo.java
@@ -0,0 +1,16 @@
+package com.caregiver.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "photo")
+@DiscriminatorValue("photo")
+public class Photo extends Contents {
+
+}
