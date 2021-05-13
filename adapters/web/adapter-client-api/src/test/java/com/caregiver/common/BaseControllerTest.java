29cf500fd59739d8ec1b567b4a4ad02801d4b829
truman-show <ljh86091@gmail.com>
2021-05-14 01:14:27 +0900

    Spring REST Docs 설정 및 회원가입(SignUpControllerTest) REST Docs 테스트 클래스를 추가하라

--- adapters/web/adapter-client-api/src/test/java/com/caregiver/common/BaseControllerTest.java
+++ adapters/web/adapter-client-api/src/test/java/com/caregiver/common/BaseControllerTest.java
@@ -2,23 +2,28 @@ package com.caregiver.common;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@ExtendWith(SpringExtension.class)
-public class BaseControllerTest {
-
+@ExtendWith({SpringExtension.class, RestDocumentationExtension.class})
+public abstract class BaseControllerTest {
   protected MockMvc mockMvc;
 
   @BeforeEach
-  void setUp(WebApplicationContext webApplicationContext) {
+  void setUp(WebApplicationContext webApplicationContext,
+             RestDocumentationContextProvider restDocumentation) {
+
     this.mockMvc = MockMvcBuilders
         .webAppContextSetup(webApplicationContext)
+        .apply(documentationConfiguration(restDocumentation))
         .addFilters(new CharacterEncodingFilter("UTF-8", true))
         .alwaysDo(print())
         .build();
