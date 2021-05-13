0f3baa18d567d79079f75093be4f20aab13ea371
truman-show <ljh86091@gmail.com>
2021-05-14 01:14:27 +0900

    Spring REST Docs 설정 및 회원가입(SignUpControllerTest) REST Docs 테스트 클래스를 추가하라

--- adapters/web/adapter-client-api/src/test/java/com/caregiver/user/controller/SignUpControllerTest.java
+++ adapters/web/adapter-client-api/src/test/java/com/caregiver/user/controller/SignUpControllerTest.java
@@ -6,9 +6,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.ResultActions;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static com.caregiver.common.ApiDocumentUtils.getDocumentRequest;
+import static com.caregiver.common.ApiDocumentUtils.getDocumentResponse;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.JsonFieldType.BOOLEAN;
+import static org.springframework.restdocs.payload.JsonFieldType.OBJECT;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(SignUpController.class)
@@ -22,12 +32,27 @@ class SignUpControllerTest extends BaseControllerTest {
 
   @Test
   void testExample() throws Exception {
-    this.mockMvc.perform(post(URL)
-        .content("{\"userName\" : \"이지훈\", \"password\" : \"1234\"}")
-        .contentType(APPLICATION_JSON)
-        .accept(APPLICATION_JSON))
-        .andExpect(status().isCreated());
-  }
 
+    ResultActions resultActions = mockMvc.perform(RestDocumentationRequestBuilders.post(URL)
+        .content("{\"userName\" : \"이지훈\", \"password\" : \"1234\"}")
+        .contentType(MediaType.APPLICATION_JSON)
+    );
+
+    resultActions.andExpect(status().isCreated())
+        .andDo(document("sticker/make-sticker",
+            getDocumentRequest(),
+            getDocumentResponse(),
+            requestFields(
+                fieldWithPath("userName").description("유저 이름").type(STRING),
+                fieldWithPath("password").description("패스워드").type(STRING)
+            ),
+            responseFields(
+                fieldWithPath("success").description("성공 여부").type(BOOLEAN),
+                fieldWithPath("message").description("응답 메세지").type(STRING),
+                fieldWithPath("data").description("응답 데이터").type(OBJECT).optional()
+            )
+        ));
+
+  }
 
 }
