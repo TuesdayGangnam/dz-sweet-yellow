b893bb48a947a9ce1c41b63194f3968af069d88b
김용진/결제플랫폼팀 <yongjin.kim@yanolja.com>
2021-05-20 10:17:29 +0900

    모임 API 구현 > 모임 생성 하기를 구현하라

---
+++ adapters/web/adapter-client-api/src/main/java/com/caregiver/user/controller/GroupController.java
@@ -0,0 +1,30 @@
+package com.caregiver.user.controller;
+
+import com.caregiver.user.dto.GroupDto;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 모임 컨트롤러
+ */
+@RequestMapping("/v1/group")
+public class GroupController {
+
+  /**
+   * 모임 생성을 요청합니다.
+   *
+   * @param request 모임생성 요청
+   */
+  @PostMapping("")
+  public ResponseEntity create(
+      @RequestBody @Valid GroupDto.createRequest request) {
+
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .build();
+  }
+}
---
+++ adapters/web/adapter-client-api/src/main/java/com/caregiver/user/dto/GroupDto.java
@@ -0,0 +1,15 @@
+package com.caregiver.user.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GroupDto {
+
+  @Getter
+  @NoArgsConstructor
+  public static class createRequest {
+
+  }
+}
