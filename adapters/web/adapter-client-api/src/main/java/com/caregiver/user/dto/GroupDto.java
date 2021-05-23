151b4c943652d9eee1e41ab2e8730e7a7d5d763d
김용진/결제플랫폼팀 <yongjin.kim@yanolja.com>
2021-05-24 01:08:57 +0900

    GroupDto final 추가

--- adapters/web/adapter-client-api/src/main/java/com/caregiver/user/dto/GroupDto.java
+++ adapters/web/adapter-client-api/src/main/java/com/caregiver/user/dto/GroupDto.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,22 +12,16 @@ import lombok.NoArgsConstructor;
 public class GroupDto {
 
   @Getter
-  @NoArgsConstructor
+  @AllArgsConstructor
   public static class CreateRequest {
 
     @NotBlank
-    private String groupName;
+    private final String groupName;
 
     @Min(value = 3, message = "최소 3인 이상 설정 가능합니다.")
     @Max(value = 10, message = "최대 10인 이상만 설정 가능합니다.")
-    private int capacity;
+    private final int capacity;
 
-    private String description;
-
-    public CreateRequest(String groupName, int capacity, String description) {
-      this.groupName = groupName;
-      this.capacity = capacity;
-      this.description = description;
-    }
+    private final String description;
   }
 }
