26bec82abc7a4b74ac51bafdbca1317d315f4fee
김용진/결제플랫폼팀 <yongjin.kim@yanolja.com>
2021-05-24 12:22:02 +0900

    그룹 컨트롤러를 생성하라

---
+++ adapters/web/adapter-client-api/src/main/java/com/caregiver/user/controller/GroupController.java
@@ -0,0 +1,32 @@
+package com.caregiver.user.controller;
+
+import com.caregiver.user.dto.GroupDto;
+import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 모임 컨트롤러
+ */
+@RestController
+@RequestMapping("/v1/group")
+public class GroupController {
+
+    /**
+     * 모임 생성을 요청합니다.
+     *
+     * @param request 모임생성 요청
+     */
+    @PostMapping("")
+    public ResponseEntity create(
+            @RequestBody @Valid GroupDto.CreateRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .build();
+    }
+}
