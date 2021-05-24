61117c43c587df326ed58ab04ad5fb4c69128e43
김용진/결제플랫폼팀 <yongjin.kim@yanolja.com>
2021-05-24 11:17:49 +0900

    그룹 API 에 필요한 GroupDto 를 추가하라

---
+++ adapters/web/adapter-client-api/src/main/java/com/caregiver/user/dto/GroupDto.java
@@ -0,0 +1,27 @@
+package com.caregiver.user.dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class GroupDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class CreateRequest {
+
+        @NotBlank
+        private final String groupName;
+
+        @Min(value = 3, message = "최소 3인 이상 설정 가능합니다.")
+        @Max(value = 10, message = "최대 10인 이상만 설정 가능합니다.")
+        private final int capacity;
+
+        private final String description;
+    }
+}
