0bdd3315b6c4e6018518696d3fc89964ad9cce4b
truman-show <ljh86091@gmail.com>
2021-06-12 20:04:55 +0900

    SNS Application Junit Test를 실행하기 위한 클래스를 추가하라

---
+++ adapters/notification/adapter-aws-sns/src/test/java/com/caregiver/AdapterSnsApplicationTest.java
@@ -0,0 +1,10 @@
+package com.caregiver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SNS Application Junit Test를 실행하기 위한 클래스.
+ */
+@SpringBootApplication
+public class AdapterSnsApplicationTest {
+}
