451c95cc5317e22c3fb595bef7bf1deb2378debd
Kimyongjin <yongjin5184@gmail.com>
2021-06-16 22:36:53 +0900

    amazonS3Config 를 추가하여 @Value bucket 을 없애라

--- adapters/persistence/adapter-s3/src/main/java/com/caregiver/port/in/UploadImageAdapter.java
+++ adapters/persistence/adapter-s3/src/main/java/com/caregiver/port/in/UploadImageAdapter.java
@@ -3,35 +3,40 @@ package com.caregiver.port.in;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.caregiver.config.AmazonS3Config;
 import com.caregiver.user.port.in.ImageUploadUseCase.RequestCommand;
 import com.caregiver.user.port.in.ImageUploadUseCase.ResponseCommand;
 import com.caregiver.user.port.out.ImageUploadPort;
 import java.io.File;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 @RequiredArgsConstructor
 public class UploadImageAdapter implements ImageUploadPort {
 
   private final AmazonS3Client amazonS3Client;
-
-  @Value("${cloud.aws.s3.bucket}")
-  private String bucket;
+  private final AmazonS3Config amazonS3Config;
 
   @Override
   public ResponseCommand upload(RequestCommand command) {
     File uploadFile = command.getUploadFile();
-    String uploadImageUrl = putS3(uploadFile, command.getDirName() + File.separator + uploadFile.getName());
+    String uploadImageUrl = putS3(
+        uploadFile,
+        amazonS3Config.getImagePath() + uploadFile.getName(),
+        amazonS3Config.getBucket()
+    );
     removeNewFile(uploadFile);
 
     return ResponseCommand.of(uploadImageUrl);
   }
 
-  private String putS3(File uploadFile, String fileName) {
-    amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
-        CannedAccessControlList.PublicRead));
+  private String putS3(File uploadFile, String fileName, String bucket) {
+    amazonS3Client.putObject(
+        new PutObjectRequest(bucket, fileName, uploadFile)
+            .withCannedAcl(CannedAccessControlList.PublicRead)
+    );
+
     return amazonS3Client.getUrl(bucket, fileName).toString();
   }
 
