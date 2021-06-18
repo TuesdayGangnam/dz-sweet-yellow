d259e96e35d50b570ccca72e1aeb548648467d8d
Kimyongjin <yongjin5184@gmail.com>
2021-06-18 22:03:22 +0900

    Java Doc 추가

--- adapters/persistence/adapter-s3/src/main/java/com/caregiver/port/in/UploadImageAdapter.java
+++ adapters/persistence/adapter-s3/src/main/java/com/caregiver/port/in/UploadImageAdapter.java
@@ -11,6 +11,9 @@ import java.io.File;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 이미지 업로드 어뎁터
+ */
 @Slf4j
 @RequiredArgsConstructor
 public class UploadImageAdapter implements ImageUploadPort {
@@ -18,6 +21,11 @@ public class UploadImageAdapter implements ImageUploadPort {
   private final AmazonS3Client amazonS3Client;
   private final AmazonS3Config amazonS3Config;
 
+  /**
+   * 파일을 업로드 합니다.
+   *
+   * @param command 파일을 업로드 하기 위한 커멘드
+   */
   @Override
   public ResponseCommand upload(RequestCommand command) {
     File uploadFile = command.getUploadFile();
@@ -31,6 +39,14 @@ public class UploadImageAdapter implements ImageUploadPort {
     return ResponseCommand.of(uploadImageUrl);
   }
 
+  /**
+   * 파일을 S3 에 업로드 합니다.
+   *
+   * @param uploadFile 업로드할 파일 객체
+   * @param fileName 업로드할 파일명
+   * @param bucket 업로드한 버킷
+   * @return 업로드한 파일의 경로
+   */
   private String putS3(File uploadFile, String fileName, String bucket) {
     amazonS3Client.putObject(
         new PutObjectRequest(bucket, fileName, uploadFile)
@@ -40,6 +56,11 @@ public class UploadImageAdapter implements ImageUploadPort {
     return amazonS3Client.getUrl(bucket, fileName).toString();
   }
 
+  /**
+   * 파일을 삭제 합니다.
+   *
+   * @param targetFile 삭제할 파일
+   */
   private void removeNewFile(File targetFile) {
     if (targetFile.delete()) {
       log.info("파일이 삭제되었습니다.");
