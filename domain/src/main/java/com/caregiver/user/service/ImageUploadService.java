496ee509c65e5980b869f98a0a42f9728db5f9fe
Kimyongjin <yongjin5184@gmail.com>
2021-06-18 21:52:00 +0900

    TODO 추가

--- domain/src/main/java/com/caregiver/user/service/ImageUploadService.java
+++ domain/src/main/java/com/caregiver/user/service/ImageUploadService.java
@@ -14,7 +14,7 @@ public class ImageUploadService implements ImageUploadUseCase {
   @Override
   public ResponseCommand upload(RequestCommand command) {
     imageUploadPort.upload(command);
-
+    // TODO: 응답값을 정의한 후 반환하는 코드 작성
     return ResponseCommand.of("임시업로드URL");
   }
 }
