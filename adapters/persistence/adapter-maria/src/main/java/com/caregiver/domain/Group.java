751b981a075e60341a95260746d2ce17aa591d40
truman-show <65605968+truman-show@users.noreply.github.com>
2021-06-26 22:59:50 +0900

    컴파일 에러를 해결하라 (#57)
    - 존재하지 않는 User 객체를 참고로 인해 컴파일 에러가 발생하고있습니다.
    - Member 객체로 변경했습니다.

--- adapters/persistence/adapter-maria/src/main/java/com/caregiver/domain/Group.java
+++ adapters/persistence/adapter-maria/src/main/java/com/caregiver/domain/Group.java
@@ -1,48 +1,77 @@
 package com.caregiver.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Group Entity.
+ */
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
 public class Group {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "group_id")
-    private long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "group_id")
+  private long id;
 
-    @Column(name = "group_name", nullable = false)
-    private String groupName;
+  @Column(name = "group_name", nullable = false)
+  private String groupName;
 
-    @Column(name = "minimum_capacity", nullable = false)
-    private Integer minimumCapacity;
+  @Column(name = "minimum_capacity", nullable = false)
+  private Integer minimumCapacity;
 
-    @Column(name = "maximum_capacity", nullable = false)
-    private Integer maximumCapacity;
+  @Column(name = "maximum_capacity", nullable = false)
+  private Integer maximumCapacity;
 
-    @Column(name = "number_of_participants", nullable = false)
-    private Integer numberOfParticipants;
+  @Column(name = "number_of_participants", nullable = false)
+  private Integer numberOfParticipants;
 
-    @Column(name = "description")
-    private String description;
+  @Column(name = "description")
+  private String description;
 
-    @Column(name = "deleted", nullable = false)
-    private Boolean deleted;
+  @Column(name = "deleted", nullable = false)
+  private Boolean deleted;
 
-    @Column(name = "closed", nullable = false)
-    private Boolean closed;
+  @Column(name = "closed", nullable = false)
+  private Boolean closed;
 
-    @ManyToOne
-    @JoinColumn(name="user_id", referencedColumnName = "id")
-    private User user;
+  @ManyToOne
+  @JoinColumn(name = "user_id", referencedColumnName = "id")
+  private Member user;
 
-    @OneToMany(mappedBy = "contents", cascade = CascadeType.ALL)
-    @JoinColumn(name = "content_id")
-    private List<Content> contents = new ArrayList<>();
+  @OneToMany(mappedBy = "contents", cascade = CascadeType.ALL)
+  @JoinColumn(name = "content_id")
+  private List<Content> contents = new ArrayList<>();
+
+  /**
+   * Group 객체를 생성하여 리턴합니다.
+   */
+  @Builder
+  public Group(long id, String groupName, Integer minimumCapacity,
+               Integer maximumCapacity, Integer numberOfParticipants, String description,
+               Boolean deleted, Boolean closed, Member user,
+               List<Content> contents) {
+    this.id = id;
+    this.groupName = groupName;
+    this.minimumCapacity = minimumCapacity;
+    this.maximumCapacity = maximumCapacity;
+    this.numberOfParticipants = numberOfParticipants;
+    this.description = description;
+    this.deleted = deleted;
+    this.closed = closed;
+    this.user = user;
+    this.contents = contents;
+  }
 }
