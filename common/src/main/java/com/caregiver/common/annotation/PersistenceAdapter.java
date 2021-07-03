2249d42c6f21cc4fe57fa395298dff281aba919f
truman-show <ljh86091@gmail.com>
2021-07-03 22:28:20 +0900

    Persistence layer 에서 사용될 Stereo Type 애노테이션을 추가하라

---
+++ common/src/main/java/com/caregiver/common/annotation/PersistenceAdapter.java
@@ -0,0 +1,26 @@
+package com.caregiver.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Persistence layer 에서 사용될 Stereo Type 애노테이션.
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface PersistenceAdapter {
+
+  /**
+   * Stereo Type 지정.
+   */
+  @AliasFor(annotation = Component.class)
+  String value() default "";
+}
