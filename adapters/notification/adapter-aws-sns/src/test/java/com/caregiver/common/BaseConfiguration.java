a874b4fc4b27d08d9fb68ab233a2b278f70950ed
truman-show <ljh86091@gmail.com>
2021-07-18 12:03:45 +0900

    ActiveProfiles 애노테이션을 제거하라
    - 누락된 빈 클래스 정보도 추가하였습니다.

--- adapters/notification/adapter-aws-sns/src/test/java/com/caregiver/common/BaseConfiguration.java
+++ adapters/notification/adapter-aws-sns/src/test/java/com/caregiver/common/BaseConfiguration.java
@@ -5,14 +5,12 @@ import com.caregiver.config.AwsSnsClient;
 import com.caregiver.config.AwsSnsDefaultCredential;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * 테스트를 클래스에서 공통으로 필요로하는 정보를 관리하는 클래스.
  */
-@ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {AwsProperties.class, AwsSnsDefaultCredential.class,
     AwsSnsClient.class},
